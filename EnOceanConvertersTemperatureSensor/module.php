c7d3ff7fe5bfc27b2e498778ebaf6bde72fd5770
Kai Oey <kai.oey@synergetix.de>
2025-10-07 18:39:23 -0500

    fix

--- EnOceanConvertersTemperatureSensor/module.php
+++ EnOceanConvertersTemperatureSensor/module.php
@@ -375,7 +375,8 @@ class EnOceanConvertersTemperatureSensor extends IPSModuleStrict
         $form = file_get_contents(__DIR__ . '/form.json');
         // Unterstützte Devices einfügnen
 		$validModules = GUIDs::allTemperatureIpsGuids();
-        $form = str_replace('<!---VALID_MODULES-->', str_replace('"', "'", json_encode($validModules)), $form);
+		$quoted = array_map(fn($g) => "'$g'", $validModules);
+		$form = str_replace('<!---VALID_MODULES-->', '[' . implode(',', $quoted) . ']', $form);
 		$this->SendDebug(__FUNCTION__, 'GetConfigurationForm: ' . $form, 0);
 		return $form;
 	}
