5b4e7a972a002db05e198fdf8b85ac349c0ffc40
Kai Oey <kai.oey@synergetix.de>
2025-10-11 14:49:48 -0500

    UI

--- EnOceanConvertersMotionSensor/module.php
+++ EnOceanConvertersMotionSensor/module.php
@@ -448,6 +448,12 @@ class EnOceanConvertersMotionSensor extends IPSModuleStrict
 		$validModules = GUIDs::allOccupancyIpsGuids();
 		$form = str_replace('<!---VALID_MODULES-->', json_encode($validModules), $form);
 		$form = str_replace('<!---VALID_EEP_OPTIONS-->', EEPProfiles::createFormularJsonFromAvailableEEP(EEPProfiles::allMotionProfiles()), $form);
+		$form = str_replace('<!---BACKUP_TEMPERATURE_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::TEMPERATURE])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_HUMIDITY_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::HUMIDITY])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_MOTION_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::MOTION])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_ILLUMINATION_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::ILLUMINATION])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_VOLTAGE_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::VOLTAGE])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_BUTTON_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::BUTTON])==="0" ? 'true' : 'false'), $form);
 		return $form;
 	}
 }
--- EnOceanConvertersTemperatureSensor/module.php
+++ EnOceanConvertersTemperatureSensor/module.php
@@ -280,6 +280,11 @@ class EnOceanConvertersTemperatureSensor extends IPSModuleStrict
 		$form = str_replace('<!---VALID_MODULES-->', json_encode($validModules), $form);
 		$form = str_replace('<!---VALID_EEP_OPTIONS-->', EEPProfiles::createFormularJsonFromAvailableEEP(EEPProfiles::allTemperatureProfiles()), $form);
 		$form = str_replace('<!---BACKUP_TEMPERATURE_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::TEMPERATURE])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_HUMIDITY_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::HUMIDITY])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_MOTION_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::MOTION])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_ILLUMINATION_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::ILLUMINATION])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_VOLTAGE_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::VOLTAGE])==="0" ? 'true' : 'false'), $form);
+		$form = str_replace('<!---BACKUP_BUTTON_VISIBLE-->', ($this->getECBuffer(self::EEP_VARIABLES[self::BUTTON])==="0" ? 'true' : 'false'), $form);
 		return $form;
 	}
 }
